e011e37a460166c7a05117d67ff17c2edab88e5a
chriskappa <chriskagkelis@yahoo.com>
2025-02-13 11:37:59 +0000

    Added env variable and created util function for rate limiting

---
+++ utils/util.js
@@ -0,0 +1,7 @@
+import rateLimit from "express-rate-limit";
+
+export const getApiLimiter = () => rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 5000,
+    message: "Too many requests, please try again later.",
+});
