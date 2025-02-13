887064640c29e9e42dc4d74411748385e58a5742
chriskappa <chriskagkelis@yahoo.com>
2025-02-13 11:41:42 +0000

    Added mongoose db connection

---
+++ db.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+function connectDB() {
+    const url = process.env.MONGOOSE_URL;
+    try {
+        mongoose.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+    const dbConnection = mongoose.connection;
+    dbConnection.once("open", (_) => {
+        console.log(`Database connected`);
+    });
+
+    dbConnection.on("error", (err) => {
+        console.error(`connection error: ${err}`);
+    });
+    return;
+}
+
+export default connectDB;
